0e89b536e56cb4a4ee3fc27b3a1c8b137ce84e52
José Andrés Otavalo Quito <71414651+josandotavalo@users.noreply.github.com>
2022-10-31 20:55:40 -0500

    Add files via upload

---
+++ Script olsr/olsr_nodo4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#config_ad_hoc
+sudo systemctl stop dhcpcd
+sudo iwconfig wlan0 mode ad-hoc
+sudo iwconfig wlan0 essid "olsr_network"
+sudo ifconfig wlan0 20.0.20.14 netmask 255.255.255.0 up
+
+
+#start_olsrd
+sudo olsrd -i wlan0
