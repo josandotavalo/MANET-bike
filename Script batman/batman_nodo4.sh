6c49406403bbfab7663d6408eb67c95c409ed352
José Andrés Otavalo Quito <71414651+josandotavalo@users.noreply.github.com>
2022-11-07 20:38:12 -0500

    Update batman_nodo4.sh

--- Script batman/batman_nodo4.sh
+++ Script batman/batman_nodo4.sh
@@ -4,7 +4,7 @@
 sudo modprobe batman-adv
 # Disable and configure wlan0
 sudo ip link set wlan0 down
-sudo ifconfig wlan0 mtu 1468
+sudo ifconfig wlan0 mtu 1500
 sudo iwconfig wlan0 mode ad-hoc
 sudo iwconfig wlan0 essid batman-network
 sudo iwconfig wlan0 ap any
@@ -16,4 +16,4 @@ sudo batctl if add wlan0
 sleep 1s
 sudo ifconfig bat0 up
 sleep 5s
-sudo ifconfig bat0 10.0.10.14/24
+sudo ifconfig bat0 10.0.0.14/24
