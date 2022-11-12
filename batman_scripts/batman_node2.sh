5402f849af034988e73937ee8f48ef200d5b0d77
José Andrés Otavalo Quito <71414651+josandotavalo@users.noreply.github.com>
2022-11-12 10:49:44 -0500

    Add files via upload

---
+++ batman_scripts/batman_node2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Activate batman-adv
+sudo modprobe batman-adv
+# Disable and configure wlan0
+sudo ip link set wlan0 down
+sudo ifconfig wlan0 mtu 1500
+sudo iwconfig wlan0 mode ad-hoc
+sudo iwconfig wlan0 essid "batman-network"
+sudo iwconfig wlan0 ap any
+sudo iwconfig wlan0 channel 1
+sleep 3s
+sudo ip link set wlan0 up
+sleep 3s
+sudo batctl if add wlan0
+sleep 3s
+sudo ifconfig bat0 up
+sleep 3s
+sudo ifconfig bat0 10.0.0.12/24
---
+++ olsr_scripts/olsr_node4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Config mode ad-hoc
+sudo systemctl stop dhcpcd
+# Disable and configure wlan0
+sudo ip link set wlan0 down
+sudo ifconfig wlan0 mtu 1500
+sudo iwconfig wlan0 mode ad-hoc
+sudo iwconfig wlan0 essid "olsr-network"
+sudo iwconfig wlan0 ap any
+sudo iwconfig wlan0 channel 1
+sleep 3s
+sudo ip link set wlan0 up
+sleep 3s
+sudo ifconfig wlan0 20.0.0.14/24
+sleep 3s
+#start_olsrd
+sudo olsrd -i wlan0
